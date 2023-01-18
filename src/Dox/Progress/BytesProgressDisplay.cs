0779b8ec9c448a3a5787047c4d896187d4b4c77a
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-01-18 13:04:06 +0330

    feat: byte progress-bar display

---
+++ src/Dox/Progress/BytesProgressDisplay.cs
@@ -0,0 +1,50 @@
+﻿namespace Dox.Progress;
+
+public class BytesProgressDisplay : IProgress<long>
+{
+    private readonly bool _consoleCanReportProgress;
+    private readonly long _fileSize;
+
+    public BytesProgressDisplay(long fileSize)
+    {
+        _fileSize = fileSize;
+        _consoleCanReportProgress = true;
+
+        try
+        {
+            // This will throw an exception if we're running in ISE
+            var top = Console.CursorTop;
+        }
+        catch (SecurityException)
+        {
+            // No permission to mess with the console, 
+            _consoleCanReportProgress = false;
+        }
+        catch (IOException)
+        {
+            // This console doesn't allow position setting
+            _consoleCanReportProgress = false;
+        }
+    }
+
+    public void Report(long value)
+    {
+        if (_consoleCanReportProgress)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write($"{value} of {_fileSize} uploaded.");
+        }
+
+        if (value < _fileSize)
+        {
+            return;
+        }
+
+        if (!_consoleCanReportProgress)
+        {
+            Console.Write($"{value} of {_fileSize} uploaded.");
+        }
+
+        Console.Write("\n");
+    }
+}
